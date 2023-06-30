9676a47a8e5335a4d20e382c155e3a2b2a06caa2
dillmanncode <dillmanncode1999@gmail.com>
2023-06-30 20:24:58 -0300

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage : {
+        'hero' : 'url(../../assets/images/bg-hero.jpg)',
+        'star' : 'url(../../assets/svg/star-icon.svg)',
+        'info' : 'url(../../assets/svg/info-icon.svg)',
+        'play' : 'url(../../assets/svg/play-icon.svg)',
+      },
       backgroundColor : theme => ({
         ...theme('colors'),
         'myDark' : '#03020A',
+        'myRed' : '#E13C2F',
+        'myGrey' : '#333333'
       }),
       textColor : {
         'myRed' : '#E13C2F'
@@ -13,6 +21,9 @@ module.exports = {
       fontFamily : {
         Ubuntu : ['Ubuntu', 'sans-serif'],
         Bebas : ['Bebas Neue', 'sans-serif']
+      },
+      screens : {
+        '480px' : '480px'
       }
     },
   },
